e995e7bf5a0489c213d5b0fcfbe8c078f85409b8
JacobShiber <yaakovshiber@gmail.com>
2022-02-24 16:23:27 +0200

    added a few more get functions using request params

--- index.js
+++ index.js
@@ -55,6 +55,53 @@ app.get('/numbers/:index', (req, res) => {
     res.send(`${numbers} , output - ${numbers[req.params.index]}`);
 })
 
+
+//checks if the given number exist in the array
+app.get('/isNumber/:num', (req, res) => {
+    const numbers = [];
+
+    for(let i=0; i<10; i++){
+        numbers.push(Math.floor(Math.random() * 10));
+    }
+
+    let response = false;
+
+    numbers.forEach(number => {
+        if(number == req.params.num) response = true ;
+    })
+
+    if(response == true){
+        res.send(`${req.params.num} exists in the array`);
+    }
+    else {
+        res.send(`the given number dont exist in the array. \n the array - ${numbers} given number - ${req.params.num}`)
+    }
+})
+
+//checks if the given number is bigger than the random num and return the heighst number
+app.get('/bigNumber/:num', (req, res) => {
+    let randomNumber = Math.floor(Math.random() * 10);
+
+    let winner = 0;
+
+    if(randomNumber > req.params.num) {winner = randomNumber}
+    else winner = req.params.num;
+
+    res.send(`winner - ${winner}`);
+})
+
+//checks if the given number is bigger than the random num and return the smallest number
+app.get('/smallNumber/:num', (req, res) => {
+    let randomNumber = Math.floor(Math.random() * 99);
+
+    let winner = 0;
+
+    if(randomNumber < req.params.num) {winner = randomNumber}
+    else winner = req.params.num;
+
+    res.send(`winner - ${winner}`);
+})
+
 app.listen(port);
 
 
